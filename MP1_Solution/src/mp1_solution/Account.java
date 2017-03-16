9583b230dd43335b8fc244bdb1ddf6953dc06988
Chris Cannon <chris.c.cannon@gmail.com>
2017-03-16 16:04:15 -0400

    Create properties

--- MP1_Solution/src/mp1_solution/Account.java
+++ MP1_Solution/src/mp1_solution/Account.java
@@ -5,10 +5,58 @@
  */
 package mp1_solution;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author CCannon
  */
 public class Account {
+    private AccountType accountType;
+    private String accountNumber;
+    private double balance;
+    ArrayList<Transaction> transactions;
+
+    /**
+     * @return the accountType
+     */
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    /**
+     * @param accountType the accountType to set
+     */
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    /**
+     * @return the accountNumber
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @param accountNumber the accountNumber to set
+     */
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * @return the balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
     
 }
