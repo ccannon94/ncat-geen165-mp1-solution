eb85675700b1d101ad8c993e71bd7b40376f5e07
Chris Cannon <chris.c.cannon@gmail.com>
2017-03-16 17:37:48 -0400

    Update main to test level 2

--- MP1_Solution/src/mp1_solution/Main.java
+++ MP1_Solution/src/mp1_solution/Main.java
@@ -17,7 +17,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CyberBank bank = new CyberBank();
+        bank.setAddress("1601 E Market St, Greensboro, NC 27401");
+        bank.setBankName("Aggie Federal Credit Union");
+        bank.setPhoneNumber("111-222-3333");
+        
+        Customer cust1 = new Customer();
+        cust1.setFirstName("Chris");
+        cust1.setLastName("Cannon");
+        cust1.setAddress("123 Main St, Matthews, NC 28105");
+        cust1.setDob("3/10/1994");
+        cust1.setPhoneNumber("123-456-7890");
+        cust1.setPin(1234);
+        cust1.setCustomerId(11234455);
+        
         Account myAcct = new Account();
         myAcct.setAccountNumber("abc1230");
         myAcct.setAccountType(AccountType.checking);
@@ -38,7 +51,50 @@ public class Main {
         
         myAcct.addTransaction(trans2);
         
-        JOptionPane.showMessageDialog(null, myAcct.toString());
+        cust1.addAccount(myAcct);
+        
+        Account myAcct2 = new Account();
+        myAcct2.setAccountNumber("sg343f");
+        myAcct2.setAccountType(AccountType.savings);
+        
+        Transaction trans3 = new Transaction();
+        trans3.setAmount(550);
+        trans3.setDate("1/3/15");
+        trans3.setDescription("Check deposit");
+        trans3.setTransactionType(TransactionType.credit);
+        
+        myAcct2.addTransaction(trans3);
+        
+        cust1.addAccount(myAcct2);
+        
+        bank.addCustomer(cust1);
+        
+        Customer cust2 = new Customer();
+        cust2.setFirstName("Jim");
+        cust2.setLastName("Bob");
+        cust2.setAddress("321 1st St, Anytown, NC 12345");
+        cust2.setDob("11/8/1960");
+        cust2.setCustomerId(24681012);
+        cust2.setPhoneNumber("800-867-5309");
+        cust2.setPin(9876);
+        
+        Account hisAcct = new Account();
+        hisAcct.setAccountNumber("djf3r4rh84yh4");
+        hisAcct.setAccountType(AccountType.investment);
+        
+        Transaction trans4 = new Transaction();
+        trans4.setAmount(15000.35);
+        trans4.setDate("4/3/15");
+        trans4.setDescription("401k rollover");
+        trans4.setTransactionType(TransactionType.credit);
+        
+        hisAcct.addTransaction(trans4);
+        
+        cust2.addAccount(hisAcct);
+        
+        bank.addCustomer(cust2);
+        
+        JOptionPane.showMessageDialog(null, bank.toString());
     }
     
 }
