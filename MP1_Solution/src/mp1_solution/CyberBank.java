c26de05dddb2b2611ae5dba5a8315058323ab0e0
Chris Cannon <chris.c.cannon@gmail.com>
2017-03-16 16:45:07 -0400

    Encapsulate Customers arrayList

--- MP1_Solution/src/mp1_solution/CyberBank.java
+++ MP1_Solution/src/mp1_solution/CyberBank.java
@@ -73,4 +73,60 @@ public class CyberBank {
     public void setBankBalance(double bankBalance) {
         this.bankBalance = bankBalance;
     }
+    
+    /**
+     * @return number of Customer objects in ArrayList
+     */
+    public int getNumCustomers(){
+        return customers.size();
+    }
+    
+    /**
+     * @param index where requested customer is located
+     * @return Customer at given index
+     */
+    public Customer getCustomer(int index){
+        return customers.get(index);
+    }
+    
+    /**
+     * @param index to insert given Customer in ArrayList
+     * @param item Customer object to add
+     */
+    public void setCustomer(int index, Customer item){
+        if(customers.size() > index){
+            for(int i = 0; i < customers.get(i).getNumAccounts(); i ++){
+                bankBalance -= customers.get(i).getAccount(i).getBalance();
+            }
+        }
+        
+        for(int i = 0; i < item.getNumAccounts(); i ++){
+            bankBalance += item.getAccount(i).getBalance();
+        }
+        
+        customers.set(index, item);
+    }
+    
+    /**
+     * @param item Customer to add to ArrayList
+     */
+    public void addCustomer(Customer item){
+        for(int i = 0; i < item.getNumAccounts(); i ++){
+            bankBalance += item.getAccount(i).getBalance();
+        }
+        
+        customers.add(item);
+    }
+    
+    /**
+     * @param index of Customer to remove in ArrayList
+     * @return Customer that has been removed
+     */
+    public Customer deleteCustomer(int index){
+        for(int i = 0; i < customers.get(i).getNumAccounts(); i ++){
+            bankBalance -= customers.get(i).getAccount(i).getBalance();
+        }
+        
+        return customers.remove(index);
+    }
 }
