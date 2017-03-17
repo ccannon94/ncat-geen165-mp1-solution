2938a712afcbbc120d3c482d1fba127007ccfe45
Chris Cannon <chris.c.cannon@gmail.com>
2017-03-16 21:37:24 -0400

    Remove commented code left for reference

--- MP1_Solution/src/mp1_solution/Main.java
+++ MP1_Solution/src/mp1_solution/Main.java
@@ -45,13 +45,4 @@ public class Main {
         
         bank.saveBankData(outputFile);
     }
-    /*
-    JFileChooser fileChooser = new JFileChooser();
-    fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-    int result = fileChooser.showOpenDialog(this);
-    if (result == JFileChooser.APPROVE_OPTION) {
-        File selectedFile = fileChooser.getSelectedFile();
-        System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-    }
-*/
 }
